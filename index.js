fef8f9f82ccbb99b49cc3289fc177d0ecfd66d8f
wiresboy-exe <liamcbone@gmail.com>
2023-01-15 13:25:01 +0000

    add vrc support cause why not

--- index.js
+++ index.js
@@ -9,19 +9,35 @@ http.createServer((req, res) => {
     let text = decodeURIComponent(req.url.split('/?')[1]);
     if(!text)return;
 
-    notifier.SendNotification(new XSNotification({
-        Title: text,
-        MessageType: 2
-    })).then(() => { console.log('Updated Music: \n'+text) })
-
-    if(prevSong !== text && newSongNotifs){
+    if(prevSong !== text){
         prevSong = text;
 
+        client.send('/chatbox/input', [ '♫ ' + prevSong.split('(')[0].split('[')[0].trim(), true ], () => {
+            console.log('Updated VRC');
+        });
+
         notifier.SendNotification(new XSNotification({
-            Title: text,
-            UseBase64Icon: true,
-        })).then(() => {})
+            Title: text.split('(')[0].split('[')[0].trim(),
+            MessageType: 2
+        })).then(() => { console.log('Updated Music: \n'+text) })
+
+        if(newSongNotifs){
+            notifier.SendNotification(new XSNotification({
+                Title: text.split('(')[0].split('[')[0].trim(),
+                Volume: 0.2
+            })).then(() => {})
+        }
     }
 
     res.end('yes');
 }).listen(8053);
+
+// vrc shit
+const { Client } = require('node-osc');
+const client = new Client('127.0.0.1', 9000);
+
+setInterval(() => {
+    client.send('/chatbox/input', [ '♫ ' + prevSong.split('(')[0].split('[')[0].trim(), true ], () => {
+        console.log('Updated VRC');
+    });
+}, 10000);
